876c0e8c0c9ef958b90f7a260ec9e9e85eede76c
kaique <kaiqueveck@gmail.com>
2023-01-20 10:48:26 -0300

    Habits

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: ["01-01", "01-02", "01-06"],
-  water: ["01-04", "01-05"],
-  food: ["01-01", "01-03"],
-  takePills: ["01-04", "01-05"],
-  journal: ["01-01", "01-03"],
-};
+button.addEventListener("click", add);
+form.addEventListener("change", save);
 
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Dia já incluso");
+    return;
+  }
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
